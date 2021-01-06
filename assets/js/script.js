921f5ea50e32bde8fba6a3cf575ff935c9b0e046
Cooper <cooperflynn@utexas.edu>
2021-01-06 13:17:36 -0600

    add dragleave functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,6 +163,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border style: dashed;");
     }
 };
 
@@ -184,7 +185,18 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
     
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
 }
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
